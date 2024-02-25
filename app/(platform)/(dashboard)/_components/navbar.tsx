7baca788aaec998143c5da62f90c4d6fd4957036
Nikita <81682288+NaViMaTRiX@users.noreply.github.com>
2024-02-26 00:01:39 +0300

    prisma init

--- app/(platform)/(dashboard)/_components/navbar.tsx
+++ app/(platform)/(dashboard)/_components/navbar.tsx
@@ -3,11 +3,12 @@ import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
 
 import { Logo } from "@/components/logo";
 import { Button } from "@/components/ui/button";
+import { MobileSidebar } from "./mobile-sidebar";
 
 export const Navbar = () => {
     return (
         <nav className="fixed z-50 top-0 p-4 w-full h-14 border-b shadow-sm bg-white flex items-center">
-            {/* TODO: Mobile Sidebar */}
+            <MobileSidebar></MobileSidebar>
             <div className="flex items-center gap-x-4">
                 <div className="hidden md:flex">
                     <Logo />
@@ -16,11 +17,11 @@ export const Navbar = () => {
                     Create
                 </Button>
                 <Button variant="primary" size="sm" className="rounded-sm block md:hidden">
-                    <Plus className="h-4 w-4"/>
+                    <Plus className="h-4 w-4" />
                 </Button>
             </div>
             <div className="ml-auto flex items-center gap-x-2">
-                <OrganizationSwitcher 
+                <OrganizationSwitcher
                     hidePersonal
                     afterCreateOrganizationUrl="/organization/:id"
                     afterLeaveOrganizationUrl="/select-org"
@@ -35,7 +36,7 @@ export const Navbar = () => {
                         },
                     }}
                 />
-                <UserButton 
+                <UserButton
                     afterSignOutUrl="/"
                     appearance={{
                         elements: {
--- app/(platform)/(dashboard)/_components/sidebar.tsx
+++ app/(platform)/(dashboard)/_components/sidebar.tsx
@@ -13,81 +13,89 @@ import { Accordion } from "@/components/ui/accordion";
 import { NavItem, Organization } from "./nav-item";
 
 interface SidebarProps {
-    storageKey?: string;
+	storageKey?: string;
 };
 
 export const Sidebar = ({
-    storageKey = "t-sidebar-state",
+	storageKey = "t-sidebar-state",
 }: SidebarProps) => {
-    const [ expanded, setExpanded ] = useLocalStorage<Record<string, any>>(storageKey, {});
+	const [expanded, setExpanded] = useLocalStorage<Record<string, any>>(storageKey, {});
 
-    const { 
-        organization: activeOrganization,
-        isLoaded: isLoadedOrg
-    } = useOrganization();
+	const {
+		organization: activeOrganization,
+		isLoaded: isLoadedOrg
+	} = useOrganization();
 
-    const {
-        userMemberships,
-        isLoaded: isLoadedOrgList
-    } = useOrganizationList({
-        userMemberships: {
-            infinite: true,
-        },
-    });
+	const {
+		userMemberships,
+		isLoaded: isLoadedOrgList
+	} = useOrganizationList({
+		userMemberships: {
+			infinite: true,
+		},
+	});
 
-    const defaultAccordionValue: string[] = Object.keys(expanded).reduce((acc: string[], key: string) => {
-        if(expanded[key]){
-            acc.push(key);
-        }
+	const defaultAccordionValue: string[] = Object.keys(expanded).reduce((acc: string[], key: string) => {
+		if (expanded[key]) {
+			acc.push(key);
+		}
 
-        return acc;
-    }, []);
+		return acc;
+	}, []);
 
-    const onExpand = (id: string) => {
-        setExpanded((curr) => ({
-            ...curr,
-            [id]: !expanded[id],
-        }));
-    };
+	const onExpand = (id: string) => {
+		setExpanded((curr) => ({
+			...curr,
+			[id]: !expanded[id],
+		}));
+	};
 
-    if(!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading){
-        return (
-            <div className="">
-                <Skeleton />
-            </div>
-        );
-    };
+	if (!isLoadedOrg || !isLoadedOrgList || userMemberships.isLoading) {
+		return (
+			<>
+				<div className="flex items-center justify-between mb-2">
+					<Skeleton className="h-10 w-[50%]" />
+					<Skeleton className="h-10 w-10" />
+				</div>
+				<div className="space-y-2">
+					<NavItem.Skeloton />
+					<NavItem.Skeloton />
+					<NavItem.Skeloton />
+				</div>
+			</>
+		);
+	};
 
-    return (
-        <>
-            <div className="font-medium text-xs flex items-center mb-1">
-                <span className="pl-4">
-                    Workspaces
-                </span>
-                <Button
-                    asChild type="button" size="icon" variant="ghost" className="ml-auto"
-                >
-                    <Link href="/select-org">
-                        <Plus 
-                            className="h-4 w-4"
-                        />
-                    </Link>
-                </Button>
-            </div>
-            <Accordion
-                type="multiple" defaultValue={defaultAccordionValue}
-                className="space-y-2"
-            >
-                {userMemberships.data.map(({ organization }) => (
-                    <NavItem 
-                        key={organization.id}
-                        isActive={activeOrganization?.id === organization.id}
-                        isExpanded={expanded[organization.id]}
-                        organization={organization as Organization}
-                        onExpand={onExpand}
-                    />
-                ))}
-            </Accordion>
-        </>
-    )
+	return (
+		<>
+			<div className="font-medium text-xs flex items-center mb-1">
+				<span className="pl-4">
+					Workspaces
+				</span>
+				<Button
+					asChild type="button" size="icon" variant="ghost" className="ml-auto"
+				>
+					<Link href="/select-org">
+						<Plus
+							className="h-4 w-4"
+						/>
+					</Link>
+				</Button>
+			</div>
+			<Accordion
+				type="multiple" defaultValue={defaultAccordionValue}
+				className="space-y-2"
+			>
+				{userMemberships.data.map(({ organization }) => (
+					<NavItem
+						key={organization.id}
+						isActive={activeOrganization?.id === organization.id}
+						isExpanded={expanded[organization.id]}
+						organization={organization as Organization}
+						onExpand={onExpand}
+					/>
+				))}
+			</Accordion>
+		</>
+	)
 }
