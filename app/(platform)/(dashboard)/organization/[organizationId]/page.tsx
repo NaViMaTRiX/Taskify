47d6c1fb948efb073368d21faab75ff8b6b485ed
nikita <flniver2016@gmail.com>
2024-02-26 16:11:11 +0300

    create board action

--- app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/page.tsx
@@ -1,9 +1,32 @@
-const OrganizationIdPage = () => {
-    return (
-        <div className="">
-            Organization Page!
-        </div>
-    );
+import { create } from "@/actions/create-board"
+import { Button } from "@/components/ui/button";
+import { db } from "@/lib/db";
+import { Board } from "./board";
+
+const OrganizationIdPage = async () => {
+	const boards = await db.board.findMany();
+
+	return (
+		<div className="flex flex-col space-y-4">
+			<form action={create}>
+				<input 
+					type="title" 
+					name="title" 
+					required 
+					placeholder="Enter a board title" 
+					className="border-black border p-1"
+				/>
+				<Button type="submit">
+					Submit
+				</Button>
+			</form>
+			<div className="space-y-2">
+				{boards.map((board) => (
+					<Board key={board.id} title={board.title} id={board.id} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default OrganizationIdPage;
