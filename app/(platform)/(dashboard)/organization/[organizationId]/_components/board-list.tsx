7e7a3c92bc0f5cdefefa7453c592d4d5ac2f9371
nikita <flniver2016@gmail.com>
2024-03-01 12:16:08 +0300

    board items work

--- app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
+++ app/(platform)/(dashboard)/organization/[organizationId]/_components/board-list.tsx
@@ -1,8 +1,28 @@
+import { redirect } from "next/navigation";
+import { auth } from "@clerk/nextjs";
+
 import { FormPopover } from "@/components/form/form-popover";
 import { Hint } from "@/components/hint";
 import { HelpCircle, User2 } from "lucide-react"
+import { db } from "@/lib/db";
+import Link from "next/link";
 
-export const BoardList = () => {
+export const BoardList = async () => {
+	const {orgId} = auth()
+
+	if(!orgId){
+		return redirect("/select-org")
+	}
+
+	const boards = await db.board.findMany({ 
+		where: {
+			orgId 
+		},
+		orderBy: {
+			createAt: "desc"
+		}
+	});
+
 	return(
 		<div className="space-y-4">
 			<div className="flex items-center font-semibold text-lg text-neutral-700">
@@ -10,6 +30,19 @@ export const BoardList = () => {
 				Your boards
 			</div>
 			<div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-4">
+				{boards.map((board) => (
+					<Link
+						href={`/board/${board.id}`}
+						key={board.id}
+						style={{backgroundImage: `url(${board.imageThumbUrl})`}}
+						className="group relative aspect-video bg-no-repeat bg-center bg-cover bg-sky-700 rounded-sm h-full w-full p-2 overflow-hidden"
+					>
+						<div className="absolute inset-0 bg-black/30 group-hover:bg-black/40 transition"/>
+						<p className="relative font-semibold text-white">
+							{board.title}
+						</p>
+					</Link>
+				))}
 				<FormPopover side="right" sideOffset={10}>
 					<div 
 						role="button" 
