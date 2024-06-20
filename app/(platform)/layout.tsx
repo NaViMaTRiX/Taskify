047799d286fa57c3e466a94c12a7059a3b3d643e
Nikita <81682288+NaViMaTRiX@users.noreply.github.com>
2024-06-20 20:06:14 +0300

    Done fix bag localStorage

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -10,46 +10,44 @@ import { QueryProvider } from "@/components/providers/query-provider";
 import { useEffect, useState } from "react";
 import { PrimeReactProvider } from 'primereact/api';
 import "primereact/resources/themes/lara-light-cyan/theme.css";
-import dynamic from 'next/dynamic'
+import { useLocalStorage } from "usehooks-ts";
 
-const DynamicPlatformLayout = dynamic(() => import("../(platform)/layout"), { ssr: false, }
-);
-
-const PlatformLayout = ({
+export const PlatformLayout = ({
   children
 }: {
   children: React.ReactNode;
 }) => {
 
-  let darkModeValid;
-  const [darkMode, setDartMode] = useState<boolean | null>(JSON.parse(localStorage.getItem("theme") || "false"));
+  // let darkModeValid;
+  // const [darkMode, setDartMode] = useState<boolean | null>(JSON.parse(localStorage.getItem("theme") || "false"));
 
-  useEffect(() => {
-    if (typeof window !== 'undefined') {
-      const theme = localStorage.getItem("theme");
-      if (theme === "true") setDartMode(true);
-    }
-  }, []);
+  // useEffect(() => {
+  //   const theme = localStorage.getItem("theme");
+  //   if (theme === "true") setDartMode(true);
+  // }, []);
 
-  useEffect(() => {
-    if (darkMode !== null) {
-      if (darkMode) {
-        document.documentElement.classList.add("dark");
-        localStorage.setItem("theme", "true");
-      } else {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "false");
-      }
-    }
-  }, [darkMode]);
-  darkModeValid = darkMode;
+  // useEffect(() => {
+  //   if (darkMode !== null) {
+  //     if (darkMode) {
+  //       document.documentElement.classList.add("dark");
+  //       localStorage.setItem("theme", "true");
+  //     } else {
+  //       document.documentElement.classList.remove("dark");
+  //       localStorage.setItem("theme", "false");
+  //     }
+  //   }
+  // }, [darkMode]);
+  // darkModeValid = darkMode;
+
+
+  const [isDarkTheme] = useLocalStorage<boolean>("theme", false);
 
   return (
     <PrimeReactProvider>
       <ClerkProvider
         localization={ruRU}
         appearance={{
-          baseTheme: darkModeValid ? dark : experimental__simple,
+          baseTheme: isDarkTheme ? dark : experimental__simple,
           layout: {
             logoPlacement: "none",
             showOptionalFields: true,
